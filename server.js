b9a01663ecbe317ad6d7726d7cf6f3a7a83c5413
Nathan Hentges <nathanjhentges@gmail.com>
2021-04-30 20:15:41 -0500

    more refactor, added routes for html pages

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const db = require('./models');
 
@@ -18,8 +19,19 @@ const config = {
   useCreateIndex: true,
   useFindAndModify: false
 };
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/populate', config);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', config);
 
+// routes for different HTML pages
+app.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/exercise.html'));
+});
+
+app.get('/stats', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/stats.html'));
+});
+
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
